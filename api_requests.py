b17a442876c170d1b76d407b5992e84ecbb738fb
ChernyRozhok <arseniygorbanovskij@gmail.com>
2024-02-20 17:33:10 +0600

    yfyf

--- api_requests.py
+++ api_requests.py
@@ -1,8 +1,6 @@
 import time, hashlib, requests
 
-
-PID = 'u02a9862w7248nh6qvzv'              # имя профиля
-KEY = '3ec23971b645b541d50c1fb0a683e1f384bfeaca319ceec0fc91cdb26ff53654'  # секретный ключ
+from config import PID, KEY
 
 query = 'pid=' + PID + '&method=getRandItem&uts=' + str(int(time.time()))  # формируем строку параметров
 signature = hashlib.md5((query + KEY).encode())  # получаем цифровую подпись
--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 BOT_TOKEN = '6352417669:AAGWfdC0xPM6MBpDt7dCXqk3Qi8oyaRBd7I'
 chat_id = 5155478866
 
-from api_requests import PID, KEY
 PID = 'u02a9862w7248nh6qvzv'              # имя профиля
 KEY = '3ec23971b645b541d50c1fb0a683e1f384bfeaca319ceec0fc91cdb26ff53654'  # секретный ключ
 
